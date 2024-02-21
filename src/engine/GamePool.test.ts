3bcc8c48afb629b02f6b4aa3c84fcb86653d883e
Martacus <mcpkmart@hotmail.nl>
2024-02-21 16:54:53 +0100

    Started my first tests

--- src/engine/GamePool.test.ts
+++ src/engine/GamePool.test.ts
@@ -1,5 +1,8 @@
 import { expect, test } from 'bun:test';
 import GamePool from './GamePool';
+import User from '../models/User';
+
+
 
 test('Init GamePool', () => {
   const pool = new GamePool('test', true);
@@ -8,4 +11,9 @@ test('Init GamePool', () => {
   expect(pool.matches.length).toBe(0);
 });
 
-test('', () => {});
+test('should add a user to a non-existing match', () => {
+  const pool = new GamePool('test', true);
+  pool.addUser(new User('test_name', 'test_discord_name', 'test_socket_id'));
+  expect(pool.matches.length).toBe(1);  
+  expect(pool.matches[0].getUsers()[0].name).toBe('test_name');  
+});
