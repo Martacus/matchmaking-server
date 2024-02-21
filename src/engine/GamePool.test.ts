717a39e056055a8b39c0054a4a3a9a61f7b8289c
Martacus <mcpkmart@hotmail.nl>
2024-02-21 16:58:33 +0100

    Add GamePool testing

--- src/engine/GamePool.test.ts
+++ src/engine/GamePool.test.ts
@@ -17,3 +17,31 @@ test('should add a user to a non-existing match', () => {
   expect(pool.matches.length).toBe(1);  
   expect(pool.matches[0].getUsers()[0].name).toBe('test_name');  
 });
+
+test('addUser adds a user to an existing match that is not full', () => {
+  const gamePool = new GamePool('Test Pool', true);
+  const user1 = new User('test_name', 'test_discord_name', 'test_socket_id');  
+  const user2 = new User('test_name_2', 'test_discord_name_2', 'test_socket_id_2');  
+
+  gamePool.addUser(user1);
+  const match = gamePool.addUser(user2);
+
+  expect(gamePool.matches.length).toBe(1); 
+  expect(match.getUsers().includes(user2)).toBe(true);  
+});
+
+test('addUser creates a new match if existing matches are full', () => {
+  const gamePool = new GamePool('Test Pool', true);
+  const user1 = new User('test_name', 'test_discord_name', 'test_socket_id');  
+  const user2 = new User('test_name_2', 'test_discord_name_2', 'test_socket_id_2');   
+  const user3 = new User('test_name_3', 'test_discord_name_3', 'test_socket_id_3');  
+
+  gamePool.addUser(user1);
+  gamePool.addUser(user2);
+  gamePool.addUser(user3);
+
+  const newMatch = gamePool.addUser(user3);
+
+  expect(gamePool.matches.length).toBe(2); 
+  expect(newMatch.getUsers().includes(user3)).toBe(true); 
+});
